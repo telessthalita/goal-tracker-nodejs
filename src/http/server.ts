cfd73b1fa3d429524fa93bbc94d183d86b04eac0
Thalita Teles <telessthalita@gmail.com>
2024-09-11 20:07:25 -0300

    feat:  Adds functions.

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,43 @@
 import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoal } from '../functions/create-goal'
+import z from 'zod'
+import { getWeekPendingGoals } from '../functions/get-week-pending-goals' 
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.get('/pending-goals', async () => {
+  const sql = await getWeekPendingGoals()
+
+  return sql.sql
+})
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
 
 app
   .listen({
